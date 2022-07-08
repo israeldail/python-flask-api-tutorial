88eecfda2c2a84767b777d805eec59c6a3905a12
israeldail <israeldail2@gmail.com>
2022-07-08 22:40:43 +0000

    done

---
+++ src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify,request
+
+app = Flask(__name__)
+
+todos = [
+{"label": "Chores", "done": False},
+]
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.json
+    todos.append(request_body)
+    json_todo = jsonify(todos)
+    return json_todo
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
